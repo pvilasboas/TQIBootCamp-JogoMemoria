7812ae0b698daf8208e471c30e3c7475c2662be4
Pedro Roberto <pvilasboas@gmail.com>
2022-05-17 18:40:25 -0300

    end of part 5

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const cards = document.querySelectorAll('.card');
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard(){
+    if(lockBoard) return;
+    
     this.classList.add('flip');
     if(!hasFlippedCard){
         hasFlippedCard = true;
@@ -20,6 +23,18 @@ function checkForMatch(){
         return;
     }
     unFlipCards();
+}
+
+function disableCards(){
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unFlipCards(){
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500)
 }
 
 cards.forEach(card => {
